68763fa8e7a40be8f9cfb5f3ebc5cf340e06dcd8
neitiris <Veliantos@gmail.com>
2019-08-10 16:08:30 +0300

    Updates

---
+++ precache-manifest.3a1422f79fa78af7a5126122149d2d11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5da8fed5ab272215524bc1b2f14a96",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "9d1c5ec1345fe38a6c87",
+    "url": "/react_pagination/static/css/main.48f4b117.chunk.css"
+  },
+  {
+    "revision": "c52b036cd679506b634b",
+    "url": "/react_pagination/static/js/2.398ce7b9.chunk.js"
+  },
+  {
+    "revision": "9d1c5ec1345fe38a6c87",
+    "url": "/react_pagination/static/js/main.efe85873.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
